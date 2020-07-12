87ba076964dc5187873e78600adb8aaf554479da
sencer4898 <41899243+sencer4898@users.noreply.github.com>
2020-07-12 14:54:37 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -4,6 +4,6 @@ from whatsappAuto import send_message
 sched = BlockingScheduler()
 
 # Schedule job_function to be called every two hours
-sched.add_job(send_message, 'interval', minutes=30)
+sched.add_job(send_message, 'interval', seconds=10)
 
 sched.start()
